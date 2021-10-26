90edcc221fac522ac13abcea60c8c8e7e12d846f
Luis Pret <lcpret@gmail.com>
2021-10-25 22:32:15 -0300

    lists e bookmarks controllers e views menos o destroy

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,35 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: :show
 
   def index
     @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to new_list_bookmark_path(@list)
+    else
+      reder :new
+    end
+  end
+
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: %i[new create index show] do
-    resources :bookmarks, only: %i[new create show delete]
+  resources :lists, only: %i[new create index show delete] do
+    resources :bookmarks, only: %i[new create]
   end
 
 end
